9275ac9769cea110ad8ae7a4ca03101d372899a1
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-02-01 16:16:07 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,11 +2,11 @@
 int main()
 {
    float Number1 ,Number2,sum;
-   printf("Enter Number1:");
+   printf("Enter Number1:\n");
    scanf("%f",&Number1);
-   printf("Enter Number2:");
+   printf("Enter Number2:\n");
    scanf("%f",&Number2);
    sum=Number1+Number2;
-   printf("Sum of %f and %f is %f",Number1 ,Number2,sum);
+   printf("Sum of %f and %f is %f\n",Number1 ,Number2,sum);
    return 0;
 }
