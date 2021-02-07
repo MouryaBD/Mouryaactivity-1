97f7dcb937f7f5c020a20ac342157d3fd26f48d8
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-02-07 14:40:21 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,27 +1,29 @@
-//WAP to find the volume of a tromboloid using 4 functions.
+//Program  to find the volume of a tromboloid using 4 functions.
 #include<stdio.h>
 float input()
 {
-float num;
-printf("Enter the value:");
-scanf("%f",&num);
-return num;
+    float num;
+    printf("Enter the value:");
+    scanf("%f",&num);
+    return num;
 }
-float volume(float h,float d,float b)
+    float volume(float h,float d,float b)
 {
-float volume=(((h*d*b)+(d/b))/3);
-return volume;
+    float volume=(((h*d*b)+(d/b))/3);
+    return volume;
 }
-float output(float volume)
+    float output(float volume)
 {
-printf("Volume of tromboloid is %f",volume);
+    printf("Volume of tromboloid is %f",volume);
 }
-int main()
+    int main()
 {
-float h,d,b;
-h=input(h);
-d=input(d);
-b=input(b);
-output(volume);
-return 0;
+    float h,d,b,v;
+    h=input(h);
+    d=input(d);
+    b=input(b);
+    v=volume(h,d,b);
+    output(v);
+    return 0;
 }
+
