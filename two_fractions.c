dd88c624f457de34b2970d2abf821d71ff6857cf
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-02-16 08:19:39 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,25 +1,30 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
-typedef struct{
-float val_nume;
-float val_deno;
+typedef struct {
+    float val_nume;
+    float val_deno;
 }add_2_fra;
-add_2_fra sum(fra,fra);
-int main()
-{
-float nume1,deno1,nume2,deno2;
-printf("Enter the values for first fraction numeratour and denominator respectively:");
-scanf("%f%f",&nume1,&deno1);
-printf("Enter the value for second fraction numerator and denominator respectively:");
-scanf("%f%f",&nume2,&deno2);
-add_2_fra first={nume1,deno1};
-add_2_fra second ={nume2,deno2);
+add_2_fra input(int k) {
+    add_2_fra u;
+    printf("Enter the value of the numerator %d:\n", k);
+    scanf("%f", & u.val_nume);
+    printf("Enter the value of the denominator %d:\n", k);
+    scanf("%f", & u.val_deno);
+    return u;
 }
-void result(){
-fra result=sum(first,second);
-printf("result=%f/%f",result.nume,result.deno);
+float calc_2_fra(add_2_fra first, add_2_fra second) {
+    float calc_2_fra = ((first.val_nume / first.val_deno) + (second.val_nume / second.val_deno));
+    return calc_2_fra;
 }
-add_2_fra sum (fra first,fra second){
-add_2_fra result={(first.nume*second.deno)+(second.nume*first.deno),(first.deno*second.deno)};
-return result;
+float result(add_2_fra first, add_2_fra second, float calc_2_fra) {
+    printf("Addition of fraction (%f/%f) and (%f/%f) is %f \n", first.val_nume, first.val_deno, second.val_nume, second.val_deno, calc_2_fra);
+}
+int main() {
+    add_2_fra first, second;
+    float sum;
+    first = input(1);
+    second = input(2);
+    sum = calc_2_fra(first, second);
+    result(first, second, sum);
+    return 0;
 }
