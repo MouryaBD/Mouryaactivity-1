6a5f7e512487167073184e88569e02d14099f667
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-02-18 14:20:38 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -4,6 +4,7 @@ typedef struct {
     float val_nume;
     float val_deno;
 }add_2_fra;
+
 add_2_fra input(int k) {
     add_2_fra u;
     printf("Enter the value of the numerator %d:\n", k);
@@ -12,22 +13,32 @@ add_2_fra input(int k) {
     scanf("%f", & u.val_deno);
     return u;
 }
-float calc_2_fra(add_2_fra first, add_2_fra second) {
-    float calc_2_fra = (((first.val_nume*second.val_deno)+(second.val_nume*first.val_deno))/(first.val_deno*second.val_deno));
-    return calc_2_fra;
+
+int gcd(int a, int b) {
+   if (a == 0)
+      return b;
+   return gcd(b%a, a);
 }
-void result(add_2_fra first, add_2_fra second,float calc_2_fra, float nume,float deno) {
-    printf("Addition of fraction (%f/%f) and (%f/%f) is %f,that is in fraction (%f/%f) \n", first.val_nume, first.val_deno, second.val_nume, second.val_deno,calc_2_fra, nume,deno);
-}
-int main() {
-    add_2_fra first, second;
-    float sum,nume,deno;
-    first = input(1);
-    second = input(2);
-    sum = calc_2_fra(first,second);
-    nume=(first.val_nume*second.val_deno)+(second.val_nume*first.val_deno);
-    deno=(first.val_deno*second.val_deno);
-    result(first, second,sum,nume,deno);
-    return 0;
+void calc_2_fra(add_2_fra first, add_2_fra second) {
+   add_2_fra third;
+   
+   third.val_deno = gcd(first.val_deno, second.val_deno);
+    
+   third.val_deno = (first.val_deno*second.val_deno) / third.val_deno;
+   
+   third.val_nume = (first.val_nume)*(third.val_deno/first.val_deno) + (second.val_nume)*(third.val_deno/second.val_deno);
+   
+   int common_factor = gcd(third.val_nume, third.val_deno);
+   float d = third.val_deno/common_factor;
+   float n = third.val_nume/common_factor;
+   printf("Addition of fraction (%f/%f) and (%f/%f) is (%f/%f) \n", first.val_nume, first.val_deno, second.val_nume, second.val_deno, n,d);
+   
 }
 
+int main() {
+    add_2_fra first, second;
+    first = input(1);
+    second = input(2);
+    calc_2_fra(first, second);
+    return 0;
+}
