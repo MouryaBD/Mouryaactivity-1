aad932a91770544fc6b4ed3840616c9a5d854743
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-02-18 19:48:50 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,9 +1,11 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
+
 typedef struct {
     int val_nume;
     int val_deno;
-}add_2_fra;
+}
+add_2_fra;
 
 add_2_fra input(int k) {
     add_2_fra u;
@@ -15,30 +17,35 @@ add_2_fra input(int k) {
 }
 
 int gcd(int a, int b) {
-   if (a == 0)
-      return b;
-   return gcd(b%a, a);
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
 }
-void calc_2_fra(add_2_fra first, add_2_fra second) {
-   add_2_fra third;
-   
-   third.val_deno = gcd(first.val_deno, second.val_deno);
-    
-   third.val_deno = (first.val_deno*second.val_deno) / third.val_deno;
-   
-   third.val_nume = (first.val_nume)*(third.val_deno/first.val_deno) + (second.val_nume)*(third.val_deno/second.val_deno);
-   
-   int common_factor = gcd(third.val_nume, third.val_deno);
-   int d = third.val_deno/common_factor;
-   int n = third.val_nume/common_factor;
-   printf("Addition of fraction (%d/%d) and (%d/%d) is (%d/%d) \n", first.val_nume, first.val_deno, second.val_nume, second.val_deno, n,d);
-   
+
+add_2_fra calc_2_fra(add_2_fra first, add_2_fra second) {
+    add_2_fra third;
+
+    third.val_deno = gcd(first.val_deno, second.val_deno);
+
+    third.val_deno = (first.val_deno * second.val_deno) / third.val_deno;
+
+    third.val_nume = (first.val_nume) * (third.val_deno / first.val_deno) + (second.val_nume) * (third.val_deno / second.val_deno);
+
+    int common_factor = gcd(third.val_nume, third.val_deno);
+    third.val_deno = third.val_deno / common_factor;
+    third.val_nume = third.val_nume / common_factor;
+    return third;
+}
+void result(add_2_fra first, add_2_fra second, add_2_fra sum) {
+    printf("Addition of fraction (%d/%d) and (%d/%d) is (%d/%d) \n", first.val_nume, first.val_deno, second.val_nume, second.val_deno, sum.val_nume, sum.val_deno);
+
 }
 
 int main() {
-    add_2_fra first, second;
+    add_2_fra first, second, sum;
     first = input(1);
     second = input(2);
-    calc_2_fra(first, second);
+    sum = calc_2_fra(first, second);
+    result(first, second, sum);
     return 0;
 }
