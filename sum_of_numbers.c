871f43983bfaa1527177412996d4f8b2d47c8282
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-02-23 22:04:59 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,39 +1,40 @@
 //Write a program to find the sum of n different numbers using 4 functions
 #include<stdio.h>
-int arr[];
-int input_num() {
-    int num;
+float input_num() {
+    float num;
     printf("Enter the number of values to be summed :");
-    scanf("%d", & num);
+    scanf("%f", & num);
     return num;
 }
-int* input_value(int num,int arr[]){
+float* input_value(float num,float arr[]){
     int a ;
     for (a = 0; a <num; a++){
     printf("Enter the number %dth  value :",a+1);
-    scanf("%d", &arr[a]);
+    scanf("%f", &arr[a]);
     }
     return arr;
 }
-int sumof_n_num(int num,int arr[num]) {
-   int a, sum = 0;
+float sumof_n_num(float num,float arr[]) {
+   int a;
+   float sum = 0;
    for(a=0;a<num;a++){
         sum = sum + arr[a];
     }
     return sum;
 }
-void resultof_sum(int num, int sum,int*arr) {
+void resultof_sum(float num, float sum,float*arr) {
     int a; 
     printf("The listed  numbers are ");
     for(a=0;a<num;a++){
-    printf(" %d,",arr[a]);
+    printf(" %f,",arr[a]);
     }
-    printf(" whose value is summed to get %d",sum);
+    printf(" whose value is summed to get %f",sum);
 }
 int main() {
-    int num,sum,*result;
+    float sum,*result;
+    int num;
     num = input_num();
-    int arr[num] ;
+    float arr[num] ;
     result=input_value(num,arr);
     sum=sumof_n_num(num,result);
     resultof_sum( num,sum,result);
