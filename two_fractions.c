418a928f209fbe578e5399b3c48e3cc56c9e4a29
ENG19CS0188_Mourya B D <70167833+MouryaBD@users.noreply.github.com>
2021-03-06 06:29:11 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -28,18 +28,13 @@ int gcd(int a, int b) {
 
 fraction calculate(fraction first, fraction second) {
     fraction third;
-
-int mygcd = gcd(first.val_deno, second.val_deno);
-
+    int mygcd = gcd(first.val_deno, second.val_deno);
     third.val_deno = (first.val_deno * second.val_deno) / mygcd;
-
     third.val_nume = (first.val_nume) * (third.val_deno / first.val_deno) + (second.val_nume) * (third.val_deno / second.val_deno);
- int common_factor = gcd(third.val_nume, third.val_deno);
+    int common_factor = gcd(third.val_nume, third.val_deno);
     third.val_deno = third.val_deno / common_factor;
     third.val_nume = third.val_nume / common_factor;
-
-
-return third;
+    return third;
 }
 void result(fraction first, fraction second, fraction sum) {
     printf("Addition of fraction (%d/%d) and (%d/%d) is (%d/%d) \n", first.val_nume, first.val_deno, second.val_nume, second.val_deno, sum.val_nume, sum.val_deno);
